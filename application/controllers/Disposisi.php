fcd777c8e7e4b35956a948e65d48ce80fc30b386
rizadinata <56691626+rizadinata@users.noreply.github.com>
2020-10-02 16:04:17 +0700

    Update Disposisi.php

--- application/controllers/Disposisi.php
+++ application/controllers/Disposisi.php
@@ -2,6 +2,15 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Disposisi extends CI_Controller {
+	function __construct() {
+	    parent::__construct();
+	    //$this->load->Model('Minstansi');
+	    // $this->load->Model('Muser');
+	    if (($this->session->userdata('username') == NULL) || ($this->session->userdata('id_role') == NULL) || ($this->session->userdata('nama_user') == NULL))
+	    {
+	           redirect("login/logout","refresh");
+	    }
+	}
 
 	public function index()
 	{
