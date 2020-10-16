8def701b530906fccdc02a35e195b218089c103b
Aas Asrini <asrinirpl07@gmail.com>
2020-10-16 17:13:50 +0700

    update controller

--- application/controllers/Instansi.php
+++ application/controllers/Instansi.php
@@ -5,7 +5,6 @@ class Instansi extends CI_Controller {
 	function __construct() {
 	    parent::__construct();
 	    $this->load->Model('Minstansi');
-	    // $this->load->Model('Muser');
 	    if (($this->session->userdata('username') == NULL) || ($this->session->userdata('id_role') == NULL) || ($this->session->userdata('nama_user') == NULL))
 	    {
 	           redirect("login/logout","refresh");
